72909535aa213b3bc1a311fca7d021ee5122bb46
Borja Cuerva <borjacuerva@gmail.com>
2024-06-08 23:04:57 +0200

    chore(mock): add moks for grid and products

---
+++ mocks/gridItems.ts
@@ -0,0 +1,44 @@
+export const gridItems = [
+  {
+    id: 0,
+    title: 'Camisetas',
+    imgUrl: '/img/camisetas/camiseta-azul.jpg',
+    alt: 'Camisetas Sportivat',
+    route: '/camisetas',
+  },
+  {
+    id: 1,
+    title: 'Complementos',
+    imgUrl: '/img/complementos/bolso-azul.jpg',
+    alt: 'Complementos Sportivat',
+    route: '/complementos',
+  },
+  {
+    id: 2,
+    title: 'Leggins',
+    imgUrl: '/img/leggins/leggin-azul.jpg',
+    alt: 'Leggins Sportivat',
+    route: '/leggins',
+  },
+  {
+    id: 3,
+    title: 'Pantalones',
+    imgUrl: '/img/pantalones/pantalon-corto.jpg',
+    alt: 'Pantalones Sportivat',
+    route: '/pantalones',
+  },
+  {
+    id: 4,
+    title: 'Sudaderas',
+    imgUrl: '/img/sudaderas/sudadera-amarilla.jpg',
+    alt: 'Sudaderas Sportivat',
+    route: '/sudaderas',
+  },
+  {
+    id: 5,
+    title: 'Tops',
+    imgUrl: '/img/tops/top-gris.jpg',
+    alt: 'Tops Sportivat',
+    route: '/tops',
+  },
+]
