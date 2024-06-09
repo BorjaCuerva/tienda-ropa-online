7c07d47438301cde3375495899236eba4311a431
Borja Cuerva <borjacuerva@gmail.com>
2024-06-09 20:33:53 +0200

    chore(mocks): add mock for carousel items

---
+++ mocks/carouselItems.ts
@@ -0,0 +1,12 @@
+export const carouselItems = [
+  {
+    id: 1,
+    imageUrl: '/img/banners/banner-promo-sportivat.jpg',
+    alt: 'Banner promo sportivat Fashion Week',
+  },
+  {
+    id: 2,
+    imageUrl: '/img/banners/banner-promo-sportivat-second.jpg',
+    alt: 'Banner promo sportivat',
+  },
+]
