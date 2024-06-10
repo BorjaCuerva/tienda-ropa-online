a902d9f128a4f0aabdafccadddc96d57c85b9d8a
Borja Cuerva <borjacuerva@gmail.com>
2024-06-10 22:06:45 +0200

    chore(mocks): add data

---
+++ interfaces/imageCollection.ts
@@ -0,0 +1,23 @@
+interface ImageCollection {
+  banners?: {
+    [key: string]: string
+  }
+  camisetas?: {
+    [key: string]: string
+  }
+  complementos?: {
+    [key: string]: string
+  }
+  leggins?: {
+    [key: string]: string
+  }
+  pantalones?: {
+    [key: string]: string
+  }
+  sudaderas?: {
+    [key: string]: string
+  }
+  tops?: {
+    [key: string]: string
+  }
+}
