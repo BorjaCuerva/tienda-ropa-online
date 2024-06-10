2f4036972f9240488829906fbf511bcee49759a6
BorjaCuerva <77273470+BorjaCuerva@users.noreply.github.com>
2024-06-10 09:52:51 +0000

    Deploying to gh-pages from @ BorjaCuerva/tienda-ropa-online@3ddd5c0e173e3e62c4e79a07852fe1c156371568 🚀

---
+++ assets/DzRtFyKO.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./Ci81mX3T.js";const t={};function o(r,a){return n(),c("h1",null,"TIENDA")}const _=e(t,[["render",o]]);export{_ as default};
--- constants/rrss.ts
+++
@@ -1,5 +0,0 @@
-export const INSTAGRAM =
-  'https://www.instagram.com/_sportivat?igsh=am85b2ZpMHZqaDZ5&utm_source=qr'
-export const FACEBOOK = 'https://www.instagram.com/your-username'
-export const TIKTOK = 'https://www.tiktok.com/@_sportivat?_t=8n0dvH0ww9x&_r=1'
-export const TWITTER = 'https://x.com/_sportivat?s=11&t=mnz9VNhRxT_hkf7HFo-iUg'
