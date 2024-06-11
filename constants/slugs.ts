5afafe0bbf5cc5c6816c93881f174d959f226aff
Borja Cuerva <borjacuerva@gmail.com>
2024-06-11 23:18:12 +0200

    chore(constants): Add constants for shop slugs

---
+++ constants/slugs.ts
@@ -0,0 +1,39 @@
+export const HOME = '/'
+export const SHOP = '/tienda'
+export const T_SHIRT_SHOP = 'tienda-camisetas'
+export const ACCESSORIES_SHOP = 'tienda-complementos'
+export const LEGGINGS_SHOP = 'tienda-leggins'
+export const PANTS_SHOP = 'tienda-pantalones'
+export const SWEATSHIRT_SHOP = 'tienda-sudaderas'
+export const TOPS_SHOP = 'tienda-tops'
+
+export const STORE_SLUGS = [
+  {
+    slug: SHOP,
+    name: 'Tienda',
+  },
+  {
+    slug: T_SHIRT_SHOP,
+    name: 'Camisetas',
+  },
+  {
+    slug: ACCESSORIES_SHOP,
+    name: 'Complementos',
+  },
+  {
+    slug: LEGGINGS_SHOP,
+    name: 'Leggins',
+  },
+  {
+    slug: PANTS_SHOP,
+    name: 'Pantalones',
+  },
+  {
+    slug: SWEATSHIRT_SHOP,
+    name: 'Sudaderas',
+  },
+  {
+    slug: TOPS_SHOP,
+    name: 'Tops',
+  },
+]
