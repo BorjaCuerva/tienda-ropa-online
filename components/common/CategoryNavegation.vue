0a553971ff7f1ce77744c9818c22a8451e4a78e2
Borja Cuerva <borjacuerva@gmail.com>
2024-06-11 23:19:39 +0200

    feat(components): Add CategoryNavegation component

---
+++ components/common/CategoryNavegation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { STORE_SLUGS } from '@/constants/slugs'
+const CategoryButton = defineAsyncComponent(
+  () => import('~/components/common/CategoryButton.vue'),
+)
+</script>
+<template>
+  <section class="navigation">
+    <CategoryButton v-for="slugs in STORE_SLUGS" :slug="slugs.slug">{{
+      slugs.name
+    }}</CategoryButton>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.navigation {
+  @include flex();
+  background-color: #0f0f0f;
+  padding: 1.5em 0;
+  margin: 2em 0;
+}
+</style>
