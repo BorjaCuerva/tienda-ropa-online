81b5082b92b8134419dd86d4f369d4f1168c63be
Borja Cuerva <borjacuerva@gmail.com>
2024-06-11 23:18:45 +0200

    chore(components): Add addToCart functionality to Card.vue

--- components/Card.vue
+++ components/Card.vue
@@ -9,16 +9,25 @@ type Props = {
 }
 
 defineProps<Props>()
+
+const addToCart = () => {
+  alert('Added to cart')
+}
 </script>
 <template>
   <article>
     <h2>{{ title }}</h2>
     <p>{{ price }}</p>
     <p>{{ category }}</p>
+
+    <NuxtLink class="button" @click="addToCart">Añadir al carrito</NuxtLink>
     <NuxtLink
       class="button"
-      :to="{ name: 'tienda-category-title', params: { title: slug, category } }"
-      >Comprar</NuxtLink
+      :to="{
+        name: 'tienda-category-title',
+        params: { title: slug, category },
+      }"
+      >Más información</NuxtLink
     >
   </article>
 </template>
@@ -28,5 +37,7 @@ defineProps<Props>()
   background-color: crimson;
   padding: 1em 2em;
   width: 10em;
+  margin: 0.5em 0;
+  cursor: pointer;
 }
 </style>
