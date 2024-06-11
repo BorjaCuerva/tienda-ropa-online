dd1e606c20599c5625b73c6db0786f148174b249
Borja Cuerva <borjacuerva@gmail.com>
2024-06-11 23:19:04 +0200

    feat(components): Add CategoryButton component

---
+++ components/common/CategoryButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+type Props = {
+  slug: string
+}
+
+defineProps<Props>()
+</script>
+<template>
+  <NuxtLink class="button" :to="slug"><slot></slot></NuxtLink>
+</template>
+
+<style scoped lang="scss">
+.button {
+  background-color: map.get($colors, 'c-primary');
+  padding: 1em 2em;
+  margin: 0 0.8em;
+  cursor: pointer;
+  border-radius: 0.4em;
+  color: map.get($colors, 'c-black');
+  font-weight: 800;
+  transition: all 0.3s ease;
+  min-width: 14em;
+  max-width: 14em;
+  text-align: center;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+}
+</style>
