2ac7375b7976dea74a133d91e84805ead43b07a1
Borja Cuerva <borjacuerva@gmail.com>
2024-06-12 23:49:26 +0200

    feat(components): optimize card component layout and styles

--- components/Card.vue
+++ components/Card.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+const MainButton = defineAsyncComponent(
+  () => import('@/components/common/MainButton.vue'),
+)
+
 type Props = {
   title: string
   price: number
@@ -8,36 +12,73 @@ type Props = {
   slug: string
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
 
-const addToCart = () => {
-  alert('Added to cart')
-}
+const styleCard = ref({
+  backgroundImage: `url(${props.imgUrl})`,
+})
 </script>
 <template>
-  <article>
-    <h2>{{ title }}</h2>
-    <p>{{ price }}</p>
-    <p>{{ category }}</p>
-
-    <NuxtLink class="button" @click="addToCart">Añadir al carrito</NuxtLink>
-    <NuxtLink
-      class="button"
-      :to="{
-        name: 'tienda-category-title',
-        params: { title: slug, category },
-      }"
-      >Más información</NuxtLink
-    >
+  <article class="card" :style="styleCard">
+    <div class="card__container">
+      <article class="card__info">
+        <h2 class="card__title">{{ title }}</h2>
+        <span>{{ size }}</span>
+        <span class="card__price">{{ price }}€</span>
+      </article>
+      <article class="card__buttons">
+        <MainButton>Añadir al carrito</MainButton>
+        <MainButton
+          :to="{
+            name: 'tienda-category-title',
+            params: { title: slug, category },
+          }"
+          >Más información</MainButton
+        >
+      </article>
+    </div>
   </article>
 </template>
 
 <style scoped lang="scss">
-.button {
-  background-color: crimson;
-  padding: 1em 2em;
-  width: 10em;
-  margin: 0.5em 0;
-  cursor: pointer;
+.card {
+  @include flex(column, center, flex-end);
+  margin: 1em;
+  width: 30em;
+  height: 40em;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  border-radius: 0.5em;
+
+  &__container {
+    background-color: rgba(0, 0, 0, 0.8);
+    width: 100%;
+    padding: 1em 2em;
+  }
+
+  &__info {
+    @include flex();
+    padding: 1em;
+    gap: 1em;
+    border-radius: 0.5em;
+    width: 100%;
+    text-align: center;
+  }
+
+  &__title {
+    font-size: 1.5em;
+  }
+
+  &__price {
+    font-size: 2em;
+    font-weight: bold;
+    color: map.get($colors, 'c-primary');
+  }
+
+  &__buttons {
+    @include flex(column);
+    gap: 1em;
+  }
 }
 </style>
