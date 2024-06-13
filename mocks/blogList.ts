8ed0ab73d83557c314dd665f2e459da923742df5
Borja Cuerva <borjacuerva@gmail.com>
2024-06-13 23:20:13 +0200

    chore(mocks): add mock data for blog list

---
+++ mocks/blogList.ts
@@ -0,0 +1,40 @@
+import { ClothesBlog, SportBlog, RunningBlog, RoutineBlog } from '@/public/index'
+
+export const blogs = [
+  {
+    title: 'Cuidado y Mantenimiento de tu Ropa Deportiva',
+    description:
+      'Descubre cómo mantener tu ropa deportiva en perfectas condiciones con nuestros consejos prácticos',
+    slug: 'cuidado-ropa-deportiva',
+    date: '06-01-2024',
+    alt: 'Camisetas Sportivat',
+    imgUrl: ClothesBlog,
+  },
+  {
+    title: '¡Prepárate para el Gran Evento Deportivo en Madrid!',
+    description:
+      'Madrid está lista para recibir a los aficionados en un año repleto de eventos deportivos de gran calibre',
+    slug: 'evento-deportivo-madrid',
+    date: '16-03-2024',
+    alt: 'Camisetas Sportivat',
+    imgUrl: SportBlog,
+  },
+  {
+    title: 'Equipamiento Esencial para Correr',
+    description:
+      'Para prepararte para correr, es crucial tener el equipo adecuado que te permita disfrutar de tus sesiones de manera cómoda y segura',
+    slug: 'equipamiento-para-correr',
+    date: '22-04-2024',
+    alt: 'Camisetas Sportivat',
+    imgUrl: RunningBlog,
+  },
+  {
+    title: 'Rutinas de Entrenamiento para Mejorar tu Rendimiento',
+    description:
+      'Para mejorar tu rendimiento deportivo, es fundamental implementar rutinas de entrenamiento bien estructuradas y personalizadas a tus necesidades específicas',
+    slug: 'rutinas-entrenamiento-rendimiento',
+    date: '11-5-2024',
+    alt: 'Camisetas Sportivat',
+    imgUrl: RoutineBlog,
+  },
+]
