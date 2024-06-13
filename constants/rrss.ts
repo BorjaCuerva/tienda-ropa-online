3a305b8fe0445761c8e852e512cf9458fd364964
Borja Cuerva <borjacuerva@gmail.com>
2024-06-13 23:19:41 +0200

    chore(constants): update Facebook link in rrss.ts

--- constants/rrss.ts
+++ constants/rrss.ts
@@ -1,5 +1,5 @@
 export const INSTAGRAM =
   'https://www.instagram.com/_sportivat?igsh=am85b2ZpMHZqaDZ5&utm_source=qr'
-export const FACEBOOK = 'https://www.instagram.com/your-username'
+export const FACEBOOK = 'https://www.facebook.com/share/dqfahbeT4jUzovzf/?mibextid=LQQJ4d'
 export const TIKTOK = 'https://www.tiktok.com/@_sportivat?_t=8n0dvH0ww9x&_r=1'
 export const TWITTER = 'https://x.com/_sportivat?s=11&t=mnz9VNhRxT_hkf7HFo-iUg'
